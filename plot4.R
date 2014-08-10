8578340f62005ef545b8a8879db96708b28d70b5
datjpa <jula2181@yahoo.de>
2014-08-10 20:26:51 +0200

    update2

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ with(needed_data,{
                  lines(needed_data$datetime, needed_data$Sub_metering_1)
                  lines(needed_data$datetime, needed_data$Sub_metering_2, col = "red")
                  lines(needed_data$datetime, needed_data$Sub_metering_3, col = "blue")
-                 legend("topright", lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.5)
+                 legend("topright", lty=c(1,1), col = c("black", "red", "blue"), bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=1)
         plot(needed_data$datetime, needed_data$Global_reactive_power, type="n", xlab = "datetime", ylab = "Global_reactive_power")
                 lines(needed_data$datetime, needed_data$Global_reactive_power)
         
